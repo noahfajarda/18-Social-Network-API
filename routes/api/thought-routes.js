da0139bf063015725ef03a22c82b33c2e1c36ddc
Noah Fajarda <noahfajarda@gmail.com>
2023-02-10 09:45:20 -0800

    added comment

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -12,6 +12,7 @@ router.get('/', (req, res) => {
 //TODO: ROUTE TO CREATE A NEW THOUGHT
 router.post('/', async (req, res) => {
     try {
+        // 'thoughtText', 'username'
         const thought = await Thought.create(req.body);
         res.status(200).json(thought);
     } catch (err) {
